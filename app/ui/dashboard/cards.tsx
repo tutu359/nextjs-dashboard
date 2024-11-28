50a6d1cc550fe7e68754a84b4ddfdfd386a2e7b6
tutu-wsl <tutu359@216.com>
2024-11-28 18:01:37 +0800

    1

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -17,15 +17,10 @@ export default async function CardWrapper() {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+      <Card title="Collected" value={totalPaidInvoices} type="collected"/>
+      <Card title="Pending" value={totalPendingInvoices} type="pending"/>
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices"/>
+      <Card title="Total Customers" value={numberOfCustomers} type="customers"/>
     </>
   );
 }
